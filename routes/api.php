af4e4bc42f7a337eeaad02f5b4804539974a3b40
Michael Ortega <91635577+maik10000@users.noreply.github.com>
2025-01-15 20:15:01 -0500

    listo

--- routes/api.php
+++ routes/api.php
@@ -8,19 +8,16 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::group([
-
-    'middleware' => 'api',
-    'prefix' => 'auth'
-
-], function ($router) {
-
-    Route::post('login', 'App\Http\Controllers\AuthController@login');
-    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
-    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
-    Route::post('me', 'App\Http\Controllers\AuthController@me');
+Route::prefix('auth')->middleware('auth:api')->group(function(){
+    
+    Route::controller(AuthController::class)->group(function(){
+    
+        Route::post('register','register')->name('user.register');
+        
+    });
 
 });
+
 
 Route::controller(PhotoController::class)->group(function(){
 
